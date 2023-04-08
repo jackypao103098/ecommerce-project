377927572a47515013bf340016428428fdb87fdf
jackypao <jacky.pao@iisigroup.com>
2023-04-08 22:01:31 +0800

    initial commit

---
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from './../../common/product';
+import { Component, OnInit } from '@angular/core';
+
+type NewType = OnInit;
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements NewType {
+  [x: string]: any;
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
+}
