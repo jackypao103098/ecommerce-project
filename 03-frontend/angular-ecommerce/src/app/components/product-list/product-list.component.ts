f1cd5b4c4d2bf7b529731034a1235395bb88b8fb
DESKTOP-5RNCVTQ\88698 <jackypao103098@gmail.com>
2023-04-13 09:30:14 +0800

    backend list

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,28 +1,47 @@
-import { ProductService } from 'src/app/services/product.service';
-import { Product } from './../../common/product';
 import { Component, OnInit } from '@angular/core';
-
-type NewType = OnInit;
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  // templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css'],
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements NewType {
-  [x: string]: any;
-  products: Product[] = [];
+export class ProductListComponent implements OnInit {
 
-  constructor(private productService: ProductService) {}
-  ngOnInit(): void {
-    this.listProducts();
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
-    });
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
+
 }
